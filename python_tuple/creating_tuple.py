81892f375efe5792d1de198bfecaf977ad4baf2d
moksh <mokshsharma06@gmail.com>
2025-08-28 12:05:08 +0530

    tuples in python

---
+++ python_tuple/creating_tuple.py
@@ -0,0 +1,14 @@
+number = (1,2,3,4,5)
+print(number[0])
+
+role = ("Azure", "Data", "Engineer")
+cloud, field, job = role
+print(cloud)  # Azure
+print(field)  # Data
+print(job)    # Engineer
+
+t = (5, 10, 5, 20, 5, 30)
+print(t.count(5))  # 3 times
+
+t = (10, 20, 30, 20, 40)
+print(t.index(20))  # 1 (first occurrence)
