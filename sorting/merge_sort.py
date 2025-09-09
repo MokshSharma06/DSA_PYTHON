0b364e340c3eea8c91e861c5cf13f34abc770f87
moksh <mokshsharma06@gmail.com>
2025-09-09 11:30:56 +0530

    add merge sort

---
+++ sorting/merge_sort.py
@@ -0,0 +1,25 @@
+def merge(left, right):
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+    return merge(left_half, right_half)
+
+# Example
+arr = [38, 100, 27, 43, 3, 9, 82, 10]
+print("Sorted array:", merge_sort(arr))
