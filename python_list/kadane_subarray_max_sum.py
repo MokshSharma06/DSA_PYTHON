b6f4e9a65452d5f586eae38ecf4c3721a949d392
moksh <mokshsharma06@gmail.com>
2025-09-23 11:48:06 +0530

    robust kadane

--- python_list/kadane_subarray_max_sum.py
+++ python_list/kadane_subarray_max_sum.py
@@ -11,3 +11,18 @@ def kadane(arr):
 
 arr=[-2,1,-3,4,-1,2,1,-5,4]
 print(kadane(arr))
+
+
+# more robust and stick to it 
+
+def robust(arr):
+    current_sum = arr[0]
+    maximum_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(num, current_sum + num)
+        maximum_sum = max(maximum_sum, current_sum)
+    return maximum_sum
+
+arr=[-2,1,-3,4,-1,2,1,-5,4]
+print(robust(arr))
