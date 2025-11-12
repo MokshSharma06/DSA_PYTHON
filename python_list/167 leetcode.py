c99d1780db44a97e56416f65d903a6520e04fe7c
moksh <mokshsharma06@gmail.com>
2025-11-12 11:54:24 +0530

    done 167 leetcode

---
+++ python_list/167 leetcode.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n =len(numbers)
+        i =0
+        j =n-1
+
+        while(i<j):
+            sum =numbers[i]+numbers[j]
+
+            if sum ==target:
+                return [i+1,j+1]
+            elif sum<target:
+                i+=1
+            else:j-=1
+
+        return -1
+
